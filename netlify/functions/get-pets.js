825b43b86ae6436be4079154e0081d89a21c25cf
kenny-io <ekeneeze3@gmail.com>
2021-12-07 21:55:56 +0400

    might work, or not

--- netlify/functions/get-pets.js
+++ netlify/functions/get-pets.js
@@ -1,7 +1,5 @@
-// import { supabase } from "../../src/lib/supabase";
 import { createClient } from "@supabase/supabase-js";
-// console.log(process.env["SUPABASE_URL"]);
-// console.log(process.env.SUPABASE_URL);
+
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
