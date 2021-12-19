d7d5cadb428a1d78893f11bc014d06d518b21917
kenny-io <ekeneeze3@gmail.com>
2021-12-19 20:15:42 +0400

    update config file

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev
-  ? "http://localhost:8888"
-  : "https://petscanloveeachother.netlify.app";
+export const server = dev ? "http://localhost:8888" : "https://petscan.date";
